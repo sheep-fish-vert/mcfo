7061945a2df8240889e52815c29337411eb7879a
vjxzebra <vjxzebra@gmail.com>
2016-06-22 16:19:15 +0300

    база

--- wp-content/themes/mcfo/header.php
+++ wp-content/themes/mcfo/header.php
@@ -1,12 +1,10 @@
+<?php $template_url = get_bloginfo('template_url'); ?>
 <html <?php language_attributes(); ?> class="no-js">
     <head>
             <meta charset="<?php bloginfo( 'charset' ); ?>">
             <meta name="viewport" content="width=device-width">
             <link rel="profile" href="http://gmpg.org/xfn/11">
             <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-            <!--[if lt IE 9]>
-            <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
-            <![endif]-->
             <?php wp_head(); ?>
     </head>
 
--- wp-content/themes/mcfo/index.php
+++ wp-content/themes/mcfo/index.php
@@ -1,3 +1,8 @@
 <?php
 get_header();
+$template_url = get_bloginfo('template_url');
+$include_url = TEMPLATEPATH . '/partials/';
+//=====partials===========
+include $include_url . 'block1.php';
+//=====partials===========
 get_footer();
