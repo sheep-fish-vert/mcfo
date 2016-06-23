b3d894c1ecad162822237568d2a1eba41bfff510
vjxzebra <vjxzebra@gmail.com>
2016-06-23 16:07:44 +0300

    наработки

--- wp-content/themes/mcfo/header.php
+++ wp-content/themes/mcfo/header.php
@@ -3,10 +3,141 @@
     <head>
             <meta charset="<?php bloginfo( 'charset' ); ?>">
             <meta name="viewport" content="width=device-width">
+            <meta id="viewport" name="viewport" content="width=1200,user-scalable=yes">
+
+            <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,600,400italic,600italic,700,700italic,800italic,800&subset=cyrillic,latin' rel='stylesheet' type='text/css'>
+            <link rel="stylesheet" href="http://test154.sheep.fish/wp-content/themes/mcfo/css/style.css" >
             <link rel="profile" href="http://gmpg.org/xfn/11">
             <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
             <?php wp_head(); ?>
     </head>
 
 <body <?php body_class(); ?>>
+    
+    <header>
+        <div class="header">
+            <div class="logo">
+                <a href="" class="top-logo">
+                    <img src="http://test154.sheep.fish/wp-content/themes/mcfo/images/logo.jpg" alt="">
+                </a>
+                <div class="slogan">Слоган слоган слоган слоган слоган слоган слоган слоган слоган </div>
+            </div>
+            <div class="row">
+                <nav class="conteiner">
+                    <ul class="head-nav">
+                        <li> 
+                            <a href="#"> <span> о нас </span></a>
+                            <div class="drop-dovn">
+                                <div class="conteiner">
+                                    <ul>
+                                        <li>
+                                            <a href="#">Управляющий комитет</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Маркетинг комитет</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">устав</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">положения</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">этика</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">партнеры</a>
+                                        </li>                            
+                                    </ul> 
+                                </div>
+                            </div>
+                        </li>
+                        <li> 
+                            <a href="#"> <span>новости </span></a>
+                            <div class="drop-dovn">
+                                <div class="conteiner">
+                                    <ul>
+                                        <li>
+                                            <a href="#">анонсы</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">видео</a>
+                                        </li>                         
+                                    </ul> 
+                                </div>
+                            </div>
+                        </li>
+                        <li> 
+                            <a href="#"> <span>мероприятия </span></a>
+                            <div class="drop-dovn">
+                                <div class="conteiner">
+                                    <ul>
+                                        <li>
+                                            <a href="#">календарь</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">бизнес завтрак</a>
+                                        </li>   
+                                        <li>
+                                            <a href="#">дебаты</a>
+                                        </li>                         
+                                    </ul> 
+                                 </div>
+                            </div>
+                        </li>
+                        <li> 
+                            <a href="#"> <span>офисы </span></a>
+                            <div class="drop-dovn">
+                                <div class="conteiner">
+                                    <ul>
+                                        <li>
+                                            <a href="#">семинар/лекции</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">тур</a>
+                                        </li>   
+                                        <li>
+                                            <a href="#">премия</a>
+                                        </li>   
+                                        <li>
+                                            <a href="#">гид</a>
+                                        </li>  
+                                        <li>
+                                            <a href="#">аналитика</a>
+                                        </li>                        
+                                    </ul> 
+                                </div>
+                            </div>
+                        </li>
+                        <li> 
+                            <a href="#"> <span>знак качества </span></a>
+                            <div class="drop-dovn">
+                                <div class="conteiner">
+                                    <ul class="conteiner">
+                                        <li>
+                                            <a href="#">для девелопера</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">репутация компании</a>
+                                        </li>   
+                                        <li>
+                                            <a href="#">по материалу</a>
+                                        </li>   
+                                        <li>
+                                            <a href="#">по услугам</a>
+                                        </li>                         
+                                    </ul>
+                                </div>  
+                            </div>
+
+                        </li>
+                        <li> 
+                            <a href="#"> <span>вакансии</span></a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
     <div class="global_wrapper">
+        
--- wp-content/themes/mcfo/partials/block1.php
+++ wp-content/themes/mcfo/partials/block1.php
@@ -1,2 +1,33 @@
-the hyinya
-tyt
+<div class="main_slider">
+    <div class="conteiner">
+        <div class="sliding">
+            <div class="slider-item" style="background-image: url('http://test154.sheep.fish/wp-content/themes/mcfo/images/main_sl_1.jpg');">
+                <div class="con">
+                    <div class="named">Lorem Ipsum </div>
+                    <div class="after"> Lorem Ipsum является стандартной "рыбой" для текстов на латинице <br/> с начала XVI века.</div>
+                    <a href="#" class="button"> 
+                        <span>Подробнее</span>
+                    </a>
+                </div>
+            </div>
+             <div class="slider-item" style="background-image: url('http://test154.sheep.fish/wp-content/themes/mcfo/images/main_sl_1.jpg');">
+                <div class="con">
+                    <div class="named">Lorem Ipsum </div>
+                    <div class="after"> Lorem Ipsum является стандартной "рыбой" для текстов на латинице <br/> с начала XVI века.</div>
+                    <a href="#" class="button"> 
+                        <span>Подробнее</span>
+                    </a>
+                </div>
+            </div>
+             <div class="slider-item" style="background-image: url('http://test154.sheep.fish/wp-content/themes/mcfo/images/main_sl_1.jpg');">
+                <div class="con">
+                    <div class="named">Lorem Ipsum </div>
+                    <div class="after"> Lorem Ipsum является стандартной "рыбой" для текстов на латинице <br/> с начала XVI века.</div>
+                    <a href="#" class="button"> 
+                        <span>Подробнее</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
